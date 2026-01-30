cde5dc9ec41624c0ae153979382a30bbeb688d37
Long <longs3515@gmail.com>
2026-01-30 16:22:10 +0700

    Format query và thêm order by

--- ss7_spring_data/src/main/java/com/example/ss7_spring_data/repository/iBlogRepository.java
+++ ss7_spring_data/src/main/java/com/example/ss7_spring_data/repository/iBlogRepository.java
@@ -8,36 +8,38 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface iBlogRepository extends JpaRepository<Blog, Integer> {
     @Query("""
-        select new com.example.ss7_spring_data.dto.ViewDto(
-            b.id,
-            b.title,
-            b.content,
-            c.name
-        )
-        from Blog b
-        join b.category c
-    """)
+                select new com.example.ss7_spring_data.dto.ViewDto(
+                    b.id,
+                    b.title,
+                    b.content,
+                    c.name
+                )
+                from Blog b
+                join b.category c
+            """)
     Page<ViewDto> findAllView(Pageable pageable);
 
     @Query("""
-        select new com.example.ss7_spring_data.dto.ViewDto(
-            b.id,
-            b.title,
-            b.content,
-            c.name
-        )
-        from Blog b
-        join b.category c
-        where
-            (:title is null or b.title like concat('%', :title, '%'))
-        and (:content is null or b.content like concat('%', :content, '%'))
-        and (:categoryId is null or c.id = :categoryId)
-    """)
+                select new com.example.ss7_spring_data.dto.ViewDto(
+                    b.id,
+                    b.title,
+                    b.content,
+                    c.name
+                )
+                from Blog b
+                join b.category c
+                where
+                    (:title is null or b.title like concat('%', :title, '%'))
+                and (:content is null or b.content like concat('%', :content, '%'))
+                and (:categoryId is null or c.id = :categoryId)
+                order by b.id desc
+            """)
     Page<ViewDto> search(
             @Param("title") String title,
             @Param("content") String content,
